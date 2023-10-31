74140b0af5f2059cd17d0b37969326a07b5c32ad
Andrei de Souza <71538656+andreisou@users.noreply.github.com>
2023-10-31 06:16:49 -0300

    used php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=1280, initial-scale=1.0">
+        <title>GBosta, seu portal de estrume</title>
+    </head>
+    <body>
+        <div class="blog-article">
+            <div class="blog-article-author">
+                <img src="" alt="">
+<p class="blog-article-author-name"><?php echo "Andrei de Souza" ?></p>
+            </div>
+            <h1>Jovem homossexual quase cai do server ao ter overdose de ritalina!</h1>
+            <hr>
+            <p>Em Rio do Sul, Santa Catarina, um jovem de 17 anos quase perdeu</p>
+            <p>conexão com o servidor ao tomar cerca de 4 vezes a dose prescrita.</p>
+            <p>Parece que, por estar pressionado, acabou tomando dois comprimidos</p>
+            <p>de ritalina, quando sua dose deveria ser apenas meio.</p>
+            <p>Jovens teorizaram sua aparental morte, dizendo coisas como "Morreu</p>
+            <p>mas sobreviveu" e "Tem mó cara de gay"</p>
+        </div>
+    </body>
+</html>
